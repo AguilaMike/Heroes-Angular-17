2614ee9dac2834670d8cf2b3c8af1546e57a58b7
David Hernandez <davidmiguelh@gmail.com>
2023-11-16 08:01:16 -0600

    started project initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./templates/pages/main-app/main-app.component').then(m => m.MainAppComponent),
+    children: [
+      { path: '', redirectTo: 'pipes', pathMatch: 'full' },
+      { path: '**', redirectTo: 'heroes'},
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/templates/pages/main-app/main-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './main-app.component.html',
+  styleUrl: './main-app.component.scss'
+})
+export class MainAppComponent {}
